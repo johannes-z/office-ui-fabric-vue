f95c71ae52e618364d6933f7c1781ddef1caeecf
johannes <johannes.zwirchmayr@gmail.com>
2018-04-07 22:22:14 +0200

    added underlined style to SearchBox.

--- src/components/SearchBox/searchbox.play.js
+++ src/components/SearchBox/searchbox.play.js
@@ -4,3 +4,10 @@ import { SearchBox } from './index.js'
 
 play('SearchBox')
   .add('Default', h => h(SearchBox))
+
+play('SearchBox')
+  .add('Underlined', h => h(SearchBox, {
+    props: {
+      underlined: true,
+    },
+  }))
