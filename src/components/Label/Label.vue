215b5d90f2b10c2984264f1df4d2a466ce130c39
johannes-z <johannes.zwirchmayr@gmail.com>
2018-05-18 00:44:43 +0200

    added Label component. added Spinner component.

---
+++ src/components/Label/Label.vue
@@ -0,0 +1,55 @@
+<template>
+  <label :disabled="disabled"
+         :required="required"
+         class="ms-Label root">
+    <slot />
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../common/common.scss';
+
+.root {
+  color: $ms-color-neutralPrimary;
+  box-sizing: border-box;
+  box-shadow: none;
+  margin-top: 0px;
+  margin-right: 0px;
+  margin-bottom: 0px;
+  margin-left: 0px;
+  display: block;
+  padding-top: 5px;
+  padding-right: 0px;
+  padding-bottom: 5px;
+  padding-left: 0px;
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+
+  &[disabled], &:disabled {
+    color: $ms-color-neutralTertiaryAlt;
+  }
+  &[required], &:required {
+    &:after {
+      content: " *";
+      color: $ms-color-redDark;
+      padding-right: 12px;
+    }
+  }
+}
+
+</style>
---
+++ src/components/Spinner/Spinner.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="ms-Spinner root">
+    <div :class="classObj"
+         :style="styleObj"
+         class="ms-Spinner-circle circle"/>
+
+    <div v-if="size >= 2" class="ms-Spinner-label label">
+      {{ label }}
+    </div>
+  </div>
+</template>
+
+<script>
+const SPINNER_SIZE = {
+  0: { size: '12px', name: 'xSmall' },
+  1: { size: '16px', name: 'small' },
+  2: { size: '20px', name: 'Medium' },
+  3: { size: '28px', name: 'Large' },
+}
+
+export default {
+  props: {
+    label: {
+      type: String,
+      default: null,
+    },
+    size: {
+      type: Number,
+      default: 2,
+      validator: value => SPINNER_SIZE[value] != null,
+    },
+  },
+  computed: {
+    classObj () {
+      var size = SPINNER_SIZE[this.size]
+      var obj = {}
+      obj[`ms-Spinner--${size.name}`] = true
+      return obj
+    },
+    styleObj () {
+      var size = SPINNER_SIZE[this.size]
+      var style = {}
+      style.width = size.size
+      style.height = size.size
+      return style
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../common/common.scss';
+
+@keyframes spinAnimation {
+    0% {
+      transform: rotateZ(0deg);
+    }
+    100% {
+      transform: rotateZ(360deg);
+    }
+}
+
+.circle {
+  margin-top: auto;
+  margin-right: auto;
+  margin-bottom: auto;
+  margin-left: auto;
+  box-sizing: border-box;
+
+  animation-name: spinAnimation;
+  animation-duration: 1.3s;
+  animation-iteration-count: infinite;
+  animation-timing-function: cubic-bezier(0.53, 0.21, 0.29, 0.67);
+  border-radius: 50%;
+  border-width: 1.5px;
+  border-style: solid;
+  border-color: $ms-color-themePrimary
+                $ms-color-themeLight
+                $ms-color-themeLight;
+  border-image: initial;
+}
+
+.label {
+  color: $ms-color-themePrimary;
+  margin-top: 10px;
+  text-align: center;
+}
+</style>
