59239bb1aaf98dcacca09509c28256642c4c025f
johannes-z <johannes.zwirchmayr@gmail.com>
2018-05-18 10:25:49 +0200

    fixed Toggle styles. added vue-router for component showcase.

---
+++ src/pages/Spinner.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="max-width: 300px;">
+    <h1 class="ms-font-xxl">Spinner</h1>
+
+    <Label>Extra Small Spinner</Label>
+    <Spinner :size="0" />
+
+    <Label>Small Spinner</Label>
+    <Spinner :size="1" />
+
+    <Label>Medium Spinner</Label>
+    <Spinner />
+
+    <Label>Large Spinner</Label>
+    <Spinner :size="3"/>
+
+    <Label>Spinner with Label</Label>
+    <Spinner label="I am definitely loading..." />
+
+    <Label>Large Spinner with Label</Label>
+    <Spinner :size="3" label="Seriously, still loading..." />
+  </div>
+</template>
+
+<script>
+import { Label, Spinner } from '../index'
+export default {
+  components: { Label, Spinner },
+}
+
+</script>
