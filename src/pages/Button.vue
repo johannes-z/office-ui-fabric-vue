dd29f4fb928c16031e343d67a7a473ec8efbeb52
johannes-z <johannes.zwirchmayr@gmail.com>
2018-05-26 15:38:19 +0200

    added IconButton. added is-disabled class for buttons.

--- src/pages/Button.vue
+++ src/pages/Button.vue
@@ -9,6 +9,12 @@
     <Label>Primary</Label>
     <DefaultButton :primary="true" text="Button" />
 
+    <Label>Disabled Standard</Label>
+    <DefaultButton :disabled="true" text="Button" />
+
+    <Label>Disabled Primary</Label>
+    <DefaultButton :disabled="true" :primary="true" text="Button" />
+
     <h2 class="ms-font-xl">Compound Button</h2>
     <Label>Standard</Label>
     <CompoundButton text="Create account"
@@ -18,6 +24,9 @@
     <CompoundButton :primary="true"
                     text="Create account"
                     secondary-text="You can create a new account here." />
+
+    <h2 class="ms-font-xl">Icon Button</h2>
+    <IconButton icon-name="Emoji"/>
 
     <h2 class="ms-font-xl">Action Button</h2>
     <ActionButton icon-name="AddFriend">Create account</ActionButton>
@@ -29,6 +38,7 @@ import {
   ActionButton,
   CompoundButton,
   DefaultButton,
+  IconButton,
   Label,
 } from '../index'
 export default {
@@ -36,6 +46,7 @@ export default {
     ActionButton,
     CompoundButton,
     DefaultButton,
+    IconButton,
     Label,
   },
 }
