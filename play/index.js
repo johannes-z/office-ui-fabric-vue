1c0522fe4bde22ae4c4b28fcf2a280662a631f19
johannes-z <johannes.zwirchmayr@gmail.com>
2018-07-16 14:52:16 +0200

    temporarily removed theming support. Added some TextField variants.

--- play/index.js
+++
@@ -1,6 +0,0 @@
-import '../src/css/fabric.core.css'
-
-const load = requireContext => requireContext.keys().map(requireContext)
-
-// load files which end with `.play.js` in `../src/components` folder
-load(require.context('../src/components', true, /.play.js$/))
