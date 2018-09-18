4a9a6b1e4f06af49033e22797b09342e35669909
johannes-z <johannes.zwirchmayr@gmail.com>
2018-09-18 17:57:03 +0200

    added doNotLayer.

--- src/pages/Callout.vue
+++ src/pages/Callout.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
+    <h1 class="ms-font-xxl">Callout</h1>
+
+    <h2 class="ms-font-xl">Default Callout</h2>
     <VDefaultButton ref="calloutButton"
                     @click.native="showCallout = !showCallout">
       {{ showCallout ? 'Hide callout' : 'Show callout' }}
     </VDefaultButton>
     <VCallout v-if="showCallout"
+              :do-not-layer="false"
               :target="$refs.calloutButton.$el" />
   </div>
 </template>
