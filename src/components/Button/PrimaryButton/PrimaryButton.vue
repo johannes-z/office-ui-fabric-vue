498b8c0ce97d6799bcd8ed55a8c449a8c549666e
johannes-z <johannes.zwirchmayr@gmail.com>
2018-10-14 11:52:28 +0200

    added VPrimaryButton. Fixed Panel dir.

---
+++ src/components/Button/PrimaryButton/PrimaryButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <default-button v-bind="$props"
+                  :primary="true">
+    <div class="ms-Button-textContainer">
+      <div class="ms-Button-label">
+        <slot>{{ text }}</slot>
+      </div>
+    </div>
+  </default-button>
+</template>
+
+<script>
+import DefaultButton from '../DefaultButton/DefaultButton.vue'
+
+export default {
+  components: { DefaultButton },
+  extends: DefaultButton,
+}
+</script>
