a55a603ddffaba45bad6707575cfd648c54933fc
johannes-z <johannes.zwirchmayr@gmail.com>
2018-10-17 19:46:35 +0200

    added simple modal component.

---
+++ docs/.vuepress/components/PageModal.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <v-default-button @click.native="showModal = true">
+      Open Modal
+    </v-default-button>
+
+    <v-modal :is-open="showModal"
+             @dismiss="showModal = false"
+             container-class-name="ms-modalExample-container">
+      <div class="ms-modalExample-header">
+        <span>Lorem Ipsum</span>
+      </div>
+      <div class="ms-modalExample-body">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sit
+          amet, vulputate in leo. Maecenas vulputate congue sapien eu tincidunt. Etiam eu sem turpis. Fusce tempor
+          sagittis nunc, ut interdum ipsum vestibulum non. Proin dolor elit, aliquam eget tincidunt non, vestibulum
+          ut turpis. In hac habitasse platea dictumst. In a odio eget enim porttitor maximus. Aliquam nulla nibh,
+          ullamcorper aliquam placerat eu, viverra et dui. Phasellus ex lectus, maximus in mollis ac, luctus vel
+          eros. Vivamus ultrices, turpis sed malesuada gravida, eros ipsum venenatis elit, et volutpat eros dui et
+          ante. Quisque ultricies mi nec leo ultricies mollis. Vivamus egestas volutpat lacinia. Quisque pharetra
+          eleifend efficitur.
+        </p>
+        <p>
+          Mauris at nunc eget lectus lobortis facilisis et eget magna. Vestibulum venenatis augue sapien, rhoncus
+          faucibus magna semper eget. Proin rutrum libero sagittis sapien aliquet auctor. Suspendisse tristique a
+          magna at facilisis. Duis rhoncus feugiat magna in rutrum. Suspendisse semper, dolor et vestibulum lacinia,
+          nunc felis malesuada ex, nec hendrerit justo ex et massa. Quisque quis mollis nulla. Nam commodo est
+          ornare, rhoncus odio eu, pharetra tellus. Nunc sed velit mi.
+        </p>
+        <p>
+          Sed condimentum ultricies turpis convallis pharetra. Sed sagittis quam pharetra luctus porttitor. Cras vel
+          consequat lectus. Sed nec fringilla urna, a aliquet libero. Aenean sed nisl purus. Vivamus vulputate felis
+          et odio efficitur suscipit. Ut volutpat dictum lectus, ac rutrum massa accumsan at. Sed pharetra auctor
+          finibus. In augue libero, commodo vitae nisi non, sagittis convallis ante. Phasellus malesuada eleifend
+          mollis. Curabitur ultricies leo ac metus venenatis elementum.
+        </p>
+        <p>
+          Aenean egestas quam ut erat commodo blandit. Mauris ante nisl, pellentesque sed venenatis nec, aliquet sit
+          amet enim. Praesent vitae diam non diam aliquet tristique non ut arcu. Pellentesque et ultrices eros.
+          Fusce diam metus, mattis eu luctus nec, facilisis vel erat. Nam a lacus quis tellus gravida euismod. Nulla
+          sed sem eget tortor cursus interdum. Sed vehicula tristique ultricies. Aenean libero purus, mollis quis
+          massa quis, eleifend dictum massa. Fusce eu sapien sit amet odio lacinia placerat. Mauris varius risus sed
+          aliquet cursus. Aenean lectus magna, tincidunt sit amet sodales a, volutpat ac leo. Morbi nisl sapien,
+          tincidunt sit amet mauris quis, sollicitudin auctor est.
+        </p>
+        <p>
+          Nam id mi justo. Nam vehicula vulputate augue, ac pretium enim rutrum ultricies. Sed aliquet accumsan
+          varius. Quisque ac auctor ligula. Fusce fringilla, odio et dignissim iaculis, est lacus ultrices risus,
+          vitae condimentum enim urna eu nunc. In risus est, mattis non suscipit at, mattis ut ante. Maecenas
+          consectetur urna vel erat maximus, non molestie massa consequat. Duis a feugiat nibh. Sed a hendrerit
+          diam, a mattis est. In augue dolor, faucibus vel metus at, convallis rhoncus dui.
+        </p>
+      </div>
+    </v-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      showModal: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.ms-modalExample-container {
+  height: 80vh;
+  width: 80vw;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-flow: column nowrap;
+  flex-flow: column nowrap;
+  -webkit-box-align: stretch;
+  -ms-flex-align: stretch;
+  align-items: stretch;
+}
+.ms-modalExample-container .ms-modalExample-header {
+  -webkit-box-flex: 1;
+  -ms-flex: 1 1 auto;
+  flex: 1 1 auto;
+  background: #0078d4;
+  color: #fff;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  font-size: 28px;
+  font-weight: 100;
+  font-weight: 600;
+  padding: 0 28px;
+  min-height: 40px;
+}
+.ms-modalExample-container .ms-modalExample-body {
+  -webkit-box-flex: 4;
+  -ms-flex: 4 4 auto;
+  flex: 4 4 auto;
+  padding: 5px 28px;
+  overflow-y: auto;
+}
+</style>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -74,6 +74,7 @@ module.exports = {
           collapsable: false,
           children: [
             'surfaces/layer',
+            'surfaces/modal',
             'surfaces/panel',
           ]
         },
