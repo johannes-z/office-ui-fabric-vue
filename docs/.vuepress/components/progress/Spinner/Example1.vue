723cbf4f4898e037f5d2bc2edc24a77a0893e76b
johannes-z <johannes.zwirchmayr@gmail.com>
2018-10-29 10:44:14 +0100

    Fixed Spinner name.

--- docs/.vuepress/components/progress/Spinner/Example1.vue
+++ docs/.vuepress/components/progress/Spinner/Example1.vue
@@ -1,22 +1,22 @@
 <template>
   <div style="max-width: 300px;">
     <VLabel>Extra Small Spinner</VLabel>
-    <Spinner :size="0" />
+    <VSpinner :size="0" />
 
     <VLabel>Small Spinner</VLabel>
-    <Spinner :size="1" />
+    <VSpinner :size="1" />
 
     <VLabel>Medium Spinner</VLabel>
-    <Spinner />
+    <VSpinner />
 
     <VLabel>Large Spinner</VLabel>
-    <Spinner :size="3"/>
+    <VSpinner :size="3"/>
 
     <VLabel>Spinner with Label</VLabel>
-    <Spinner label="I am definitely loading..." />
+    <VSpinner label="I am definitely loading..." />
 
     <VLabel>Large Spinner with Label</VLabel>
-    <Spinner :size="3" label="Seriously, still loading..." />
+    <VSpinner :size="3" label="Seriously, still loading..." />
   </div>
 </template>
 
