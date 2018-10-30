f379459d1393855d63146747fa4c9b6204bca8b1
johannes <johannes.zwirchmayr@gmail.com>
2018-10-30 19:46:55 +0100

    added ActivityItem.

---
+++ docs/.vuepress/components/content/ActivityItem/Example1.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <VActivityItem>
+      <template slot="icon">
+        <VIcon icon-name="Message" />
+      </template>
+      <template slot="description">
+        <VLink class="nameText">Philippe Lampros</VLink> commented
+      </template>
+      <template slot="comment">
+        Hello! I am making a comment and mentioning
+        <VLink class="nameText">@Anđela Debeljak</VLink>
+        in the text of the comment.
+      </template>
+      <template slot="timestamp">
+        Just now
+      </template>
+    </VActivityItem>
+
+    <VActivityItem>
+      <template slot="icon">
+        <VIcon icon-name="Delete" />
+      </template>
+      <template slot="description">
+        <VLink class="nameText">Lisha Refai</VLink> deleted
+        <span class="nameText">DocumentTitle.docx</span>
+      </template>
+      <template slot="timestamp">
+        2 hours ago
+      </template>
+    </VActivityItem>
+
+    <VActivityItem>
+      <template slot="icon">
+        <VIcon icon-name="FabricMovetoFolder" />
+      </template>
+      <template slot="description">
+        <VLink class="nameText">Julian Arvidsson</VLink> moved
+        <VLink class="nameText">PresentationTitle.pptx</VLink> to
+        <VLink class="nameText">Destination Folder</VLink>
+      </template>
+      <template slot="timestamp">
+        Yesterday
+      </template>
+    </VActivityItem>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.ms-ActivityItem .nameText {
+  font-weight: bold;
+}
+</style>
---
+++ src/components/ActivityItem/ActivityItem.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="ms-ActivityItem">
+    <div class="ms-ActivityItem-actitityTypeIcon">
+      <slot name="icon">
+        <div v-if="activityPersonas.length > 0"
+             class="ms-ActivityItem-personaContainer">
+          <VPersonaCoin v-for="(persona, index) in activityPersonas.slice(0, 4)"
+                        :key="index"
+                        v-bind="persona"
+                        :size="personaSize" />
+        </div>
+      </slot>
+    </div>
+
+    <div class="ms-ActivityItem-actitityContent">
+      <div class="ms-ActivityItem-activityText">
+        <slot name="description" />
+      </div>
+
+      <div class="ms-ActivityItem-commentText">
+        <slot name="comment" />
+      </div>
+
+      <div class="ms-ActivityItem-timeStamp">
+        <slot name="timestamp" />
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    activityPersonas: {
+      type: Array,
+      default () {
+        return []
+      },
+    },
+  },
+  computed: {
+    personaSize () {
+      let activityPersonas = this.activityPersonas
+      if (!activityPersonas) return 0
+      if (activityPersonas.length === 1) return 10
+      return 8
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.ms-ActivityItem {
+  margin-top: 20px;
+  font-family: "Segoe UI", "Segoe UI Web (West European)", "Segoe UI",
+    -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  font-size: 12px;
+  font-weight: 400;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  box-sizing: border-box;
+  color: rgb(102, 102, 102);
+}
+
+.ms-ActivityItem-actitityTypeIcon {
+  height: 32px;
+  font-size: 16px;
+  line-height: 16px;
+  margin-top: 3px;
+}
+
+.ms-ActivityItem-personaContainer {
+  display: flex;
+  flex-wrap: wrap;
+  min-width: 32px;
+  width: 32px;
+  height: 32px;
+}
+
+.ms-ActivityItem-actitityContent {
+  padding-top: 0px;
+  padding-right: 8px;
+  padding-bottom: 0px;
+  padding-left: 8px;
+}
+
+.ms-ActivityItem-activityText {
+  display: inline;
+}
+
+.ms-ActivityItem-commentText {
+  color: rgb(51, 51, 51);
+}
+
+.ms-ActivityItem-timeStamp {
+  font-family: "Segoe UI", "Segoe UI Web (West European)", "Segoe UI",
+    -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
+  font-size: 10px;
+  font-weight: 400;
+  color: rgb(102, 102, 102);
+}
+</style>
