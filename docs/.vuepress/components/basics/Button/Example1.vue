53a5cc8590ed7060abc14eed3a0cb54191640937
johannes <johannes.zwirchmayr@gmail.com>
2018-10-30 20:17:14 +0100

    fixed button docs.

--- docs/.vuepress/components/basics/Button/Example1.vue
+++ docs/.vuepress/components/basics/Button/Example1.vue
@@ -76,10 +76,22 @@
     <h2 class="ms-font-xl">Contextual Menu Button</h2>
 
     <h2 class="ms-font-xl">Action Button</h2>
-    <VActionButton :primary="true"
-                   icon-name="AddFriend">Create account</VActionButton>
 
-    <VActionButton icon-name="AddFriend">Create account</VActionButton>
+    <div class="ms-Grid"
+         dir="ltr">
+      <div class="ms-Grid-row">
+        <div class="ms-Grid-col ms-sm6 ms-lg3">
+          <VLabel>Standard</VLabel>
+          <VActionButton icon-name="AddFriend">Create account</VActionButton>
+        </div>
+        <div class="ms-Grid-col ms-sm6 ms-lg3">
+          <VLabel>Primary</VLabel>
+          <VActionButton :primary="true"
+                         icon-name="AddFriend">Create account</VActionButton>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
