215a3d90b1f1827d0b744daa72f7f35d60ddbbdc
johannes <johannes.zwirchmayr@gmail.com>
2018-10-30 16:09:23 +0100

    added tsconfig.json

---
+++ docs/.vuepress/components/utilities/ResizeGroup/Example1.vue
@@ -0,0 +1,76 @@
+<template>
+  <VResizeGroup :data="data"
+                :onReduceData="onReduceData"
+                :onGrowData="onGrowData"
+                @reduceData="onReduceData"
+                @growData="onGrowData">
+    <VOverflowSet :items="data.primary"
+                  :overflow-items="data.overflow.length ? data.overflow : null" />
+  </VResizeGroup>
+</template>
+
+<script>
+
+function generateData(count, cachingEnabled, checked) {
+  const icons = ['Add', 'Share', 'Upload'];
+  const dataItems = [];
+  let cacheKey = '';
+  for (let index = 0; index < count; index++) {
+    const item = {
+      key: `item${index}`,
+      name: `Item ${index}`,
+      icon: icons[index % icons.length],
+      checked: checked
+    };
+
+    cacheKey = cacheKey + item.key;
+    dataItems.push(item);
+  }
+
+  let result = {
+    primary: dataItems,
+    overflow: []
+  };
+
+  if (cachingEnabled) {
+    result = { ...result, cacheKey };
+  }
+
+  return result;
+}
+
+export default {
+  data () {
+    return {
+      data: generateData(10, false, false),
+    }
+  },
+  methods: {
+    onReduceData () {
+      let currentData = this.data
+      if (currentData.primary.length === 0) {
+        return undefined;
+      }
+
+      const overflow = [...currentData.primary.slice(-1), ...currentData.overflow];
+      const primary = currentData.primary.slice(0, -1);
+
+      return { primary, overflow };
+    },
+    onGrowData () {
+      let currentData = this.data
+      if (currentData.overflow.length === 0) {
+        return undefined;
+      }
+
+      const overflow = currentData.overflow.slice(1);
+      const primary = [...currentData.primary, ...currentData.overflow.slice(0, 1)];
+
+      return { primary, overflow };
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/ResizeGroup/ResizeGroup.vue
@@ -0,0 +1,60 @@
+<template>
+  <div ref="container">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: [Array, Object],
+      required: true,
+    },
+    onReduceData: {
+      type: Function,
+      required: true,
+    },
+    onGrowData: {
+      type: Function,
+      required: true,
+    },
+  },
+  data () {
+    return {
+      containerWidth: undefined,
+      measureContainer: true,
+      resizeDirection: 'grow',
+    }
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.onResize)
+  },
+  mounted () {
+    this.afterComponentRendered()
+    window.addEventListener('resize', this.onResize)
+  },
+  updated () {
+    this.afterComponentRendered()
+  },
+  methods: {
+    afterComponentRendered () {
+      if (this.measureContainer) {
+        this.containerWidth = this.$refs.container.getBoundingClientRect().width
+      }
+      if (this.resizeDirection === 'grow') {
+      } else {
+      }
+    },
+    remeasure () {
+      this.measureContainer = true
+    },
+    onResize (event) {
+      this.measureContainer = true
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+</style>
