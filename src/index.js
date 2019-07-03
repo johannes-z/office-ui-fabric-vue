5365709eb79becf188c63aa06cdd7917062354cc
johannes-z <johannes.zwirchmayr@gmail.com>
2019-07-03 13:13:45 +0200

    fixed plugin install

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { setSSR } from './utilities/dom.js'
 const Fabric = {
   install: function (Vue, options) {
     for (const key in components) {
-      if (!components[key]._compiled) continue
+      if (!key.startsWith('V')) continue
       Vue.component(key, components[key])
     }
   },
